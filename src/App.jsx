f040394b62af391c66b4e612c6cb5ea6a067eccf
shchurofff <shchurov.oleg27@gmail.com>
2025-05-09 14:35:40 +0300

    Удалил статичную БД с пиццами. Настроил запрос с mockApi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react'
 import { Categories } from './components/categories'
 import { Header } from './components/header'
 import { PizzaBlock } from './components/pizza-block'
 import { Sort } from './components/sort'
 import './scss/app.scss'
-import pizzas from './assets/db.json'
 
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('https://681d2344f74de1d219aee895.mockapi.io/items')
+      .then((responce) => {
+        return responce.json()
+      })
+      .then((json) => {
+        setItems(json)
+      })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,7 +29,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((pizza) => (
+            {items.map((pizza) => (
               <PizzaBlock key={pizza.id} {...pizza} />
             ))}
           </div>
